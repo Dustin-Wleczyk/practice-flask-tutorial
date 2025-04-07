8c85ec8a6225a8bd9720882d4f06f29e4be861ae
Dustin Wleczyk <dustinwleczyk@hotmail.com>
2025-04-07 03:30:15 -0400

    Finished features

--- app.py
+++ app.py
@@ -24,17 +24,43 @@ def index():
         task_content = request.form['content']
         new_task = Todo(content=task_content)
         try:
+            print("here")
             db.session.add(new_task)
+            print("here2")
             db.session.commit()
+            print("here3")
             return redirect('/')
         except:
-            return "Issue with adding task"
-
-        
+            return "Issue with adding task"    
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
     
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete= Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Issue deleting task"
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Problem updating task"
+    else:
+        return render_template('update.html', task=task)
+
 
 
 if __name__ =="__main__":
